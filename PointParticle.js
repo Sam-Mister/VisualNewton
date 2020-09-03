492440a28029546fbb4fd6b39c858a519ef36e0c
Sam Mister <sm15883@bristol.ac.uk>
2020-09-03 21:37:37 +0100

    Added a radius and restitution parameter to the particle class

--- PointParticle.js
+++ PointParticle.js
@@ -1,9 +1,11 @@
 class PointParticle{
-  constructor(x,y,v_x,v_y,mass,id){
+  constructor(x,y,v_x,v_y,mass,id,radius,restitution){
     this.pos = new Vector(x,y);
     this.vel = new Vector(v_x,v_y);
     this.mass = mass;
     this.id = id;
+    this.radius = radius;
+    this.restitution = restitution;
   }
 updatePos = function(newPos){
   this.pos = Vector.add(this.pos,newPos);
