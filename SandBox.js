6277871af082b7a581ec97315539e48d8c5ce5d7
Sam Mister <sm15883@bristol.ac.uk>
2020-09-04 22:43:00 +0100

    Added buttons and slider

--- SandBox.js
+++ SandBox.js
@@ -1,10 +1,15 @@
 let particleArr = [];
 let clicker = 0
+let button1;
+let button2;
+let cnv;
+let MassSlider;
 
 function setup() {
-  createCanvas(800, 600);
+  cnv = createCanvas(800, 600);
+  cnv.mouseClicked(addParticle);
   properties = {timestep : 1, x_min : -1*canvas.width/2, x_max : canvas.width/2, y_min : -1*canvas.height/2, y_max : canvas.height/2,
-    BoundaryConditions : "solid",
+    BoundaryConditions : "Periodic",
     TwoBodyForces : true,
     NewtonGravity : true,
     UniformGravity : false,
@@ -12,38 +17,40 @@ function setup() {
 
   world = new World(properties);
 
-  p1_render = new Circle(0,-50,0,0,100,"Particle_1",0.4)
-  world.addobject(p1_render.particle);
-  p2_render = new Circle(0,50,-0.5,0,100,"Particle_2",0.4)
-  world.addobject(p2_render.particle);
-  p3_render = new Circle(50,0,0,0.5,100,"Particle_3",0.4)
-  world.addobject(p3_render.particle);
-  p4_render = new Circle(-50,0,0,-0.5,1000,"Particle_4",0.4)
-  world.addobject(p4_render.particle);
+  button1 = createButton('Gravity');
+  button1.position(820,50);
+  button1.mousePressed(GravChange);
 
+  button2 = createButton('Forces');
+  button2.position(820,100);
+  button2.mousePressed(ForceChange);
 
+  MassSlider = createSlider(50,500,100);
+  MassSlider.position(820,150)
 }
 
 function draw() {
   translate(canvas.width/2,canvas.height/2)
   background(50);
-  p1_render.update();
-  p2_render.update();
-  p3_render.update();
-  p4_render.update();
-
   for (var i = 0; i < particleArr.length; i++){
     particleArr[i].update();
   }
   world.updateWorld();
-
 }
 
-function mouseClicked(){
-  let Pholder = new Circle((mouseX-canvas.width/2),(mouseY - canvas.height/2),0,0,200,"swarm"+clicker,0.9)
+function addParticle(){
+  let mass = MassSlider.value();
+  let Pholder = new Circle((mouseX-canvas.width/2),(mouseY - canvas.height/2),0,0,mass,"swarm"+clicker,0.9)
   particleArr.push(Pholder)
   world.addobject(Pholder.particle);
   clicker++
+}
+function GravChange(){
+  world.UniformGravity = toggle(world.UniformGravity);
+}
+
+function ForceChange(){
+  world.TwoBodyForces = toggle(world.TwoBodyForces);
 }
 
 class Circle{
